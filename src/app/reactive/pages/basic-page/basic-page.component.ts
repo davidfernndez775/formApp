c6db44d4e54e80ecc2483c7e4021709efc6a880d
davidfernndez775 <davidfernndez775@yahoo.com>
2024-08-25 21:44:13 -0400

    my first form build

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class BasicPageComponent {
+  // definimos el formulario usando FormBuilder
+  public myForm: FormGroup = this.fb.group({
+    // cada propiedad se define con la sintaxis
+    // propiedad: ['valor inicial', [validadores síncronos], [validadores asíncronas]]
+    name: [''],
+    price: [0],
+    inStorage: [0],
+  });
 
+  // inyectamos el servicio FormBuilder
+  constructor(private fb: FormBuilder) {}
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
 }
