d5edcd87cbdf1ec35ad41edd904a66046fd384cd
davidfernndez775 <davidfernndez775@yahoo.com>
2024-08-27 10:50:01 -0400

    basic form done

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -10,15 +10,19 @@ export class BasicPageComponent {
   public myForm: FormGroup = this.fb.group({
     // cada propiedad se define con la sintaxis
     // propiedad: ['valor inicial', [validadores síncronos], [validadores asíncronas]]
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   });
 
   // inyectamos el servicio FormBuilder
   constructor(private fb: FormBuilder) {}
 
+  // funcion que controla el submit del formulario
   onSave(): void {
+    // si el formulario no es válido
+    if (this.myForm.invalid) return;
+    // si el formulario es válido
     console.log(this.myForm.value);
   }
 }
